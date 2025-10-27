dbc82b44e0d3b933efb08028462822a6a6b9269a
sbx_user1051 <sbx_user1051@169.254.3.53>
2025-10-27 02:16:21 +0000

    Mon Oct 27 02:16:21 UTC 2025

--- src/main/java/io/accelerate/solutions/DMO/DemoRound3Solution.java
+++ src/main/java/io/accelerate/solutions/DMO/DemoRound3Solution.java
@@ -1,11 +1,19 @@
 package io.accelerate.solutions.DMO;
 
 import io.accelerate.runner.SolutionNotImplementedException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class DemoRound3Solution {
+    private final Map<String, InventoryItem> inventory = new HashMap<>();
+    private final Map<String, Integer> quantities = new HashMap<>();
 
     public int inventoryAdd(InventoryItem item, int quantity) {
-        throw new SolutionNotImplementedException();
+//        inventory.put(item.sku(), item);
+        int currentQuantity = quantities.getOrDefault(item.sku(), 0);
+        int newQuantity = currentQuantity + quantity;
+        quantities.put(item.sku(), newQuantity);
+        return newQuantity;
     }
 
     public int inventorySize() {
@@ -16,3 +24,4 @@ public class DemoRound3Solution {
         throw new SolutionNotImplementedException();
     }
 }
+
